a8423566cebfe9bd19521f9042d56649f33325b7
Nguyen Tran <khoinguyenit15@gmail.com>
2021-05-26 09:53:37 +0700

    demo2 useEffect

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import getEnvVars from "./config";
 import { defaultInstance } from "./api";
 
@@ -11,6 +11,8 @@ export default function App() {
     "Make sure your API server started..."
   );
 
+  const [arr, setArr] = useState([]);
+
   defaultInstance
     .get("/")
     .then((res) => {
@@ -20,9 +22,52 @@ export default function App() {
       setServerMsg("Fail to connect to server.");
     });
 
+  const [count, setCount] = useState(0);
+  const [bgButton, setbgButton] = useState("");
+
+  useEffect(() => {
+    if (count % 2 == 0) {
+      setbgButton("tomato");
+    } else {
+      setbgButton("green");
+    }
+  }, [count]);
+
+  useEffect(() => {
+    if (arr.indexOf(count) >= 0) {
+      setArr(arr.filter((x) => x !== count));
+    } else {
+      setArr([...arr, count]);
+    }
+  }, [count]);
+
+  useEffect(() => {
+    setbgButton("yellow");
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Connecting to backend server at {API_URI}</Text>
+      <Text>{count}</Text>
+      <Text>{arr.join(", ")}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      >
+        <View style={{ ...styles.button, backgroundColor: bgButton }}>
+          <Text>Count</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setCount(count - 1);
+        }}
+      >
+        <View style={{ ...styles.button, backgroundColor: bgButton }}>
+          <Text>Dec Count</Text>
+        </View>
+      </TouchableOpacity>
       <Text>Result: {serverMsg}</Text>
       <StatusBar style="auto" />
     </View>
@@ -35,5 +80,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "tomato",
+    color: "white",
+    paddingHorizontal: 30,
+    paddingVertical: 10,
   },
 });
