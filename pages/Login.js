69b4caa5605bca37fc43f4e42cd8df85b4ad831b
hawkeyescodee <liamnguyen123@gmail.com>
2021-06-03 17:55:13 +0700

    Apply basic login screen

--- pages/Login.js
+++ pages/Login.js
@@ -1,272 +1,167 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import {
-  KeyboardAvoidingView,
-  Easing,
-  StyleSheet,
   View,
-  Animated,
-  Image,
-} from "react-native";
-import {
-  CLEAR_COLOR,
-  COLD_COLOR,
-  defaultStyles,
-} from "../constants/styles";
-import {
-  Input,
   Text,
-  Button,
-  SocialIcon,
-  BottomSheet,
-  ListItem,
-} from "react-native-elements";
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Pressable,
+} from "react-native";
+import { Image, Input, Button, SocialIcon } from "react-native-elements";
+import {
+  PRIMARY_COLOR_WHITE,
+  PRIMARY_COLOR_BLACK,
+  FULL_HEIGHT,
+} from "../constants/styles";
 
-
-const ForgotPasswordSheet = (props) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const list = [
-    { title: "List Item 1" },
-    { title: "List Item 2" },
-    {
-      title: "Cancel",
-      containerStyle: { backgroundColor: "red" },
-      titleStyle: { color: "white" },
-      onPress: () => setIsVisible(false),
-    },
-  ];
-  return (
-    <BottomSheet
-      isVisible={isVisible}
-      containerStyle={{ backgroundColor: "rgba(0.5, 0.25, 0, 0.2)" }}
-    >
-      {list.map((l, i) => (
-        <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
-          <ListItem.Content>
-            <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-      ))}
-    </BottomSheet>
-  );
-};
-export default function Login({ navigation }) {
-  const loginViewBottom = useRef(new Animated.Value(-300)).current;
+const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-
-  const [userPhone, setPhone] = useState('');
-  const [userPassword, setUserPassword] = useState('');
-
-  const changePhoneHandler = (inputedPhone) => {
-    setPhone(inputedPhone);
-  };
-
-  const changePasswordHandler = (inputedPassword) => {
-    setUserPassword(inputedPassword);
-  }
-
-  const goToHome = () => {
-    navigation.navigate("HomeTab");
-  };
-
-  React.useEffect(() => {
-    Animated.timing(loginViewBottom, {
-      toValue: 0,
-      duration: 1000,
-      easing: Easing.bounce,
-    }).start();
-    // goToHome() // test
-  }, []);
-
-  const [open, setOpen] = useState(false);
-
-  const handleSubmitPress = () => {
-    // setErrortext('');
-    if (!userPhone) {
-      alert('Please fill PhoneNumber');
-      return;
-    }
-    if (!userPassword) {
-      alert('Please fill Password');
-      return;
-    }
-    let dataToSend = {phoneNumber: userPhone, password: userPassword};
-
-    let formBody = [];
-
-    for (let key in dataToSend) {
-      let encodedKey = encodeURIComponent(key);
-      let encodedValue = encodeURIComponent(dataToSend[key]);
-      formBody.push(encodedKey + '=' + encodedValue);
-    }
-    formBody = formBody.join(',');
-    //api url
-    fetch('http://localhost:3000/api/auth/login', {
-      method: 'POST',
-      body: formBody,
-      headers: {
-        //Header Defination
-        'Content-Type':
-        'application/x-www-form-urlencoded;charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-            //toDo
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-
   return (
-    <View behavior={"padding"} style={styles.container}>
-      <View style={styles.greetContainer}>
-        <View style={styles.greetTitle}>
-          <Image
-            source={require("../assets/main-icon.png")}
-            style={{ width: 200, height: 200 }}
-          />
-        </View>
+    <View style={styles.container}>
+      <View style={styles.iconHeaderContainer}>
+        <Image
+          source={require("../assets/local-runway-icon.png")}
+          style={{ width: 230, height: 220 }}
+          // PlaceholderContent={<ActivityIndicator />}
+        />
       </View>
-      <Animated.View
-        style={{
-          ...styles.loginContainer,
-          bottom: loginViewBottom,
-          left: loginViewBottom,
-        }}
-      >
-        <Text h2 style={styles.loginTitle}>
-          Sign In
-        </Text>
-        <KeyboardAvoidingView
-          style={styles.loginInputContainer}
-          behavior={"padding"}
-          keyboardVerticalOffset={10}
-        >
-          <Input
-            value={userPhone}
-            onChangeText={changePhoneHandler}
-            style={styles.input}
-            mode={"outlined"}
-            placeholder={"Phone Number"}
-            underlineColor={"red"}
-            keyboardType={"numbers-and-punctuation"}
-          />
-          <Input
-            value={userPassword}
-            onChangeText={changePasswordHandler}
-            style={styles.input}
-            secureTextEntry={!showPassword}
-            mode={"outlined"}
-            placeholder={"Password"}
-            rightIcon={{
-              type: "font-awesome-5",
-              name: showPassword ? "eye" : "eye-slash",
-              color: COLD_COLOR,
-              onPress: () => {
-                setShowPassword(!showPassword);
-              },
-            }}
-          />
-        </KeyboardAvoidingView>
-        <View style={styles.loginActionContainer}>
-          <Text
-            style={{
-              textAlign: "right",
-              fontWeight: "bold",
-              color: COLD_COLOR,
-            }}
-          >
-            Forgot Password?
-          </Text>
-          <Button
-            title={"Login"}
-            buttonStyle={defaultStyles.button}
-            titleStyle={defaultStyles.buttonTitle}
-            // onPress={goToHome}
-            onPress={handleSubmitPress}
-          />
 
-          <View style={styles.socialContainer}>
-            <Text>Or sign in using your social accounts: </Text>
-            <View style={styles.socialIcons}>
-              <SocialIcon type={"facebook"} />
-              <SocialIcon type={"google"} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.bodyContainer}
+        >
+          <View>
+            <View style={styles.bodyHeaderContainer}>
+              <Text style={styles.bodyHeader}>Log In</Text>
+            </View>
+            <Input
+              placeholder="Phone number"
+              leftIcon={{
+                type: "font-awesome",
+                name: "phone",
+                color: PRIMARY_COLOR_BLACK,
+              }}
+              keyboardType={"numeric"}
+              underlineColor={PRIMARY_COLOR_BLACK}
+              leftIconContainerStyle={{ marginHorizontal: 10 }}
+              inputContainerStyle={styles.input}
+              // onChangeText={}
+            />
+
+            <Input
+              inputContainerStyle={styles.input}
+              secureTextEntry={!showPassword}
+              // mode={"outlined"}
+              placeholder={"Password"}
+              leftIcon={{
+                type: "font-awesome",
+                name: "lock",
+                color: PRIMARY_COLOR_BLACK,
+              }}
+              leftIconContainerStyle={{ marginHorizontal: 10 }}
+              rightIcon={{
+                type: "font-awesome-5",
+                name: showPassword ? "eye" : "eye-slash",
+                color: PRIMARY_COLOR_BLACK,
+                onPress: () => {
+                  setShowPassword(!showPassword);
+                },
+              }}
+              rightIconContainerStyle={{ marginLeft: 10 }}
+            />
+            <View style={styles.forgotPwContainer}>
+              <Pressable
+                onPress={() => console.log("Press from forgot password")}
+              >
+                <Text style={{ textDecorationLine: "underline" }}>
+                  Forgot password?
+                </Text>
+              </Pressable>
             </View>
           </View>
-        </View>
-        <View style={styles.skipContainer}>
-          <Button
-            title={"Skip"}
-            onPress={goToHome}
-            icon={{
-              type: "font-awesome-5",
-              name: "paw",
-              color: COLD_COLOR,
-            }}
-            buttonStyle={[defaultStyles.button]}
-            titleStyle={defaultStyles.buttonTitle}
-          />
-        </View>
-      </Animated.View>
+
+          <View style={styles.loginContainer}>
+            <View style={styles.buttonContainer}>
+              <Button title="Login" buttonStyle={styles.button} />
+            </View>
+
+            <View style={styles.signIn}>
+              <Text style={styles.signInText}>Or Sign in with</Text>
+            </View>
+
+            <View style={styles.socialIconContainer}>
+              <SocialIcon type={"facebook"} onPress={() => console.log('Press From facebook')} />
+              <SocialIcon type={"google"} onPress={() => console.log('Press From google')} />
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    // marginTop: 15,
-    backgroundColor: "black",
+    backgroundColor: PRIMARY_COLOR_BLACK
   },
-  socialIcons: {
+  iconHeaderContainer: {
+    marginTop: 30,
     flexDirection: "row",
+    justifyContent: "center",
   },
-  loginTitle: {
-    marginLeft: 10,
-    color: COLD_COLOR,
+  bodyContainer: {
+    width: "100%",
+    marginTop: 10,
+    height: FULL_HEIGHT,
+    backgroundColor: PRIMARY_COLOR_WHITE,
+    borderTopRightRadius: 30,
+  },
+  bodyHeaderContainer: {
+    marginTop: 10,
+  },
+  bodyHeader: {
+    fontSize: 40,
     fontWeight: "bold",
-  },
-  socialContainer: {
-    marginTop: 20,
-    alignItems: "center",
-  },
-  loginActionContainer: {
-    marginTop: 10,
-  },
-  skipButton: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-  },
-  greetContainer: {
-    marginTop: -20,
-  },
-  skipContainer: {
-    marginTop: 10,
-    alignItems: "center",
+    color: PRIMARY_COLOR_BLACK,
+    marginLeft: 2,
   },
   input: {
-    backgroundColor: CLEAR_COLOR,
+    backgroundColor: PRIMARY_COLOR_WHITE,
     marginTop: 5,
+    borderWidth: 1,
+  },
+  forgotPwContainer: {
+    marginTop: -18,
+    marginLeft: 10,
   },
   loginContainer: {
-    padding: 20,
-    borderTopLeftRadius: 50,
-    backgroundColor: CLEAR_COLOR,
-    position: "absolute",
-    width: "100%",
-    left: 0,
-    bottom: "-0%",
-    shadowOffset: {},
-    shadowOpacity: 0.2,
-    shadowRadius: 20,
+    justifyContent: "center",
   },
-  loginInputContainer: {
-    marginTop: 20,
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
   },
-  greetTitle: {
-    marginTop: 5,
+  button: {
+    width: 100,
+    backgroundColor: PRIMARY_COLOR_BLACK,
+  },
+  signIn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 10,
+  },
+  signInText: {
+    color: PRIMARY_COLOR_BLACK,
+    fontWeight: "bold",
+  },
+  socialIconContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 2,
   },
 });
+
+export default Login;
