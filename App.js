1109eb0939417a0598fd433d9f6d146bb6346894
thaiqn-it <qthai20102000@gmail.com>
2021-06-15 19:38:24 +0700

    fix bug

--- App.js
+++ App.js
@@ -14,33 +14,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppStack from "./pages/stacks/AppStack";
 import { useFonts, OpenSans_400Regular } from "@expo-google-fonts/open-sans";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+const { API_URI } = getEnvVars();
 
-import { defaultInstance } from "./apis";
-import Login from "./pages/Login";
-import Splash from "./pages/Splash";
-import Product from "./pages/Product";
-
-console.disableYellowBox = true;
-
-const Stack = createStackNavigator();
 export default function App() {
-  const [serverMsg, setServerMsg] = useState(
-    "Make sure your API server started..."
-  );
-
-  const [arr, setArr] = useState([]);
-
-  defaultInstance
-    .get("/")
-    .then((res) => {
-      setServerMsg(res.data.msg);
-    })
-    .catch((err) => {
-      setServerMsg("Fail to connect to server.");
-    });
-      
   let [fontsLoaded] = useFonts({
     OpenSans_400Regular,
   });
@@ -49,11 +25,7 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Product" headerMode="none">
-        <Stack.Screen name={"Splash"} component={Splash} />
-        <Stack.Screen name={"Login"} component={Login} />
-        <Stack.Screen name={"Product"} component={Product} />
-      </Stack.Navigator>
+      <AppStack />
     </NavigationContainer>
   );
 }
